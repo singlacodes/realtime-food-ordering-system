dc77ba6fa0ff250922a138083ea2d0dcb58253d2
singlacodes <shivamsinglatechnical@gmail.com>
2026-01-02 20:36:25 +0530

    added routes for authentication and created utility for token generation

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+
 dotenv.config()
 const app=express()
 const port=process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
 app.listen(port,()=>{
     connectDb()
     console.log(`server started at ${port}`)
