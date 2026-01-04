f54e51105f372bbfc302f05237fb609ad46c89b9
singlacodes <shivamsinglatechnical@gmail.com>
2026-01-04 12:49:24 +0530

    Forgot Password issues rectified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
 
 
 export const serverUrl="http://localhost:8000"
@@ -10,6 +11,8 @@ function App() {
     <Routes>
       <Route path='/signup' element={<SignUp/>} />
       <Route path='/signin' element={<SignIn/>} />
+      <Route path='/forgot-password' element={<ForgotPassword/>} />
+
     </Routes>
   )
 }
