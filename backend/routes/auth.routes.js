16cfdc8c1ec4cf6c9ac44134ffb808b9e3d9418c
singlacodes <shivamsinglatechnical@gmail.com>
2026-01-04 13:52:53 +0530

    added route for otp

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,10 +6,9 @@ const authRouter = express.Router()
 authRouter.post("/signup", signUp)
 authRouter.post("/signin", signIn)
 authRouter.get("/signout", signOut)
-// Comment these out until you create the functions:
-// authRouter.post("/send-otp", sendOtp)
-// authRouter.post("/verify-otp", verifyOtp)
-// authRouter.post("/reset-password", resetPassword)
-// authRouter.post("/google-auth", googleAuth)
+authRouter.post("/send-otp", sendOtp)
+authRouter.post("/verify-otp", verifyOtp)
+authRouter.post("/reset-password", resetPassword)
+authRouter.post("/google-auth", googleAuth)
 
 export default authRouter
