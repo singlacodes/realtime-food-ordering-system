002612f15bf3942fae6de41d35063a8874c56281
singlacodes <shivamsinglatechnical@gmail.com>
2026-01-10 15:35:09 +0530

    Owner Redux Setup

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser"
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import userRouter from "./routes/user.routes.js"
+import shopRouter from "./routes/shop.routes.js"
+import itemRouter from "./routes/item.routes.js"
 import cors from "cors"
 
 dotenv.config()
@@ -22,6 +24,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use("/api/shop",shopRouter)
+app.use("/api/item",itemRouter)
 app.listen(port,()=>{
     connectDb()
     console.log(`server started at ${port}`)
