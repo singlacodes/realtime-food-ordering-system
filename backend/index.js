8e75457740ff07115187dedfe7aadd425dc2f217
singlacodes <shivamsinglatechnical@gmail.com>
2026-01-29 17:16:00 +0530

    changed index file and socket.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,39 @@
 import express from "express"
 import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import connectDb from "./config/db.js"
-import authRouter from "./routes/auth.routes.js"
-import userRouter from "./routes/user.routes.js"
-import shopRouter from "./routes/shop.routes.js"
-import itemRouter from "./routes/item.routes.js"
-import orderRouter from "./routes/order.routes.js"
-
-import cors from "cors"
-
 dotenv.config()
+import connectDb from "./config/db.js"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import userRouter from "./routes/user.routes.js"
+
+import itemRouter from "./routes/item.routes.js"
+import shopRouter from "./routes/shop.routes.js"
+import orderRouter from "./routes/order.routes.js"
+import http from "http"
+import { Server } from "socket.io"
+import { socketHandler } from "./socket.js"
+
 const app=express()
+const server=http.createServer(app)
+
+const io=new Server(server,{
+   cors:{
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:['POST','GET']
+}
+})
+
+app.set("io",io)
+
+
+
 const port=process.env.PORT || 5000
-
-
 app.use(cors({
-  origin: true,
-  credentials: true
-}));
-
-
-
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
@@ -29,7 +41,10 @@ app.use("/api/user",userRouter)
 app.use("/api/shop",shopRouter)
 app.use("/api/item",itemRouter)
 app.use("/api/order",orderRouter)
-app.listen(port,()=>{
+
+socketHandler(io)
+server.listen(port,()=>{
     connectDb()
     console.log(`server started at ${port}`)
 })
+
